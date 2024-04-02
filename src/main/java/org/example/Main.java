48a03b7a087c9c8ce30ec4c6e5e105c90841afdf
MatheusAurelio22 <matheusaureliolima@gmail.com>
2024-04-02 17:51:21 -0300

    atualizacao

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,41 @@ package org.example;
 
 import javax.swing.*;
 
+import static org.example.Operacional.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String nome = JOptionPane.showInputDialog("Digite seu nome");
-        JOptionPane.showMessageDialog(null,"Olá, seu nome é: "+ nome);
+
+        String numero = JOptionPane.showInputDialog("Digite um número: ");
+        int numeroDia = Integer.parseInt(numero);
+
+        switch (numeroDia){
+            case 1:
+                JOptionPane.showMessageDialog(null,"O dia da semana selecionado é Domingo");
+                break;
+            case 2:
+                JOptionPane.showMessageDialog(null,"O dia da semana selecionado é Segunda");
+                break;
+            case 3:
+                JOptionPane.showMessageDialog(null,"O dia da semana selecionado é Terça");
+                break;
+            case 4:
+                JOptionPane.showMessageDialog(null,"O dia da semana selecionado é Quarta");
+                break;
+            case 5:
+                JOptionPane.showMessageDialog(null,"O dia da semana selecionado é Quinta");
+                break;
+            case 6:
+                JOptionPane.showMessageDialog(null,"O dia da semana selecionado é Sexta");
+                break;
+            case 7:
+                JOptionPane.showMessageDialog(null,"O dia da semana selecionado é Sábado");
+                break;
+            default:
+                JOptionPane.showMessageDialog(null,"Número inválido");
+
+        }
     }
 }
